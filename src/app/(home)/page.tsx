2a6e12735ebcbec088f72b0fd8f24f2d228c3aaf
BalkyBoy <bolugafar360@gmail.com>
2025-08-19 12:13:00 +0100

    All files

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,14 +3,14 @@ import Link from 'next/link';
 export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
+      <h1 className="mb-4 text-2xl font-bold">Welcome To the DICERT Doumentation</h1>
       <p className="text-fd-muted-foreground">
         You can open{' '}
         <Link
           href="/docs"
           className="text-fd-foreground font-semibold underline"
         >
-          /docs
+          /DICERT
         </Link>{' '}
         and see the documentation.
       </p>
--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,6 +1,6 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
-
+import Image from 'next/image';
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
